70d8b206cd369c6926ef560dfcbf4e7c77bce9f8
Maciek Skokowski <maciejskok2004@gmail.com>
2024-01-13 05:17:27 +0100

    Did responsobility to 320 devices width

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ themeSwitcher.addEventListener('click', () => {
 const allOptions = document.querySelectorAll('.choose-quiz-btn');
 const chooseQuizDiv = document.querySelector('.main-choose-quiz');
 const allAnswersSection = document.querySelectorAll('.text-question-area');
+const backBtn = document.querySelector('.back-btn');
 
 const showChoosedQuiz = (event) => {
   const [html, css, js, access] = allOptions;
@@ -20,6 +21,7 @@ const showChoosedQuiz = (event) => {
   if (clickedBtn === html) {
     chooseQuizDiv.classList.add('hide');
     allAnswersSection[currentQuestion].classList.remove('hide');
+    backBtn.classList.add('active');
     generateNewQuestion();
   } else if (clickedBtn === css) {
     alert('Prace techniczne');
@@ -34,35 +36,51 @@ allOptions.forEach(btn => {
   btn.addEventListener('click', showChoosedQuiz);
 })
 
+/* ---- Back Btn ---- */
+backBtn.addEventListener('click', () => {
+  if (currentQuestion === 0) {
+    backBtn.classList.remove('active');
+    newDiv.innerHTML = '';
+    chooseQuizDiv.classList.remove('hide');
+    allAnswersSection[currentQuestion].classList.add('hide');
+  } else if (currentQuestion > 0) {
+    allAnswersSection[currentQuestion].classList.add('hide');
+    currentQuestion -= 1;
+
+    allAnswersSection[currentQuestion].classList.remove('hide');
+    generateNewQuestion();
+  }
+})
+
 /* ---- Generate Answers ---- */
+const newDiv = document.createElement('div');
 let newAnswersArr;
 let newSubmits;
 
 const generateNewQuestion = () => {
   const htmlAnswersSection = document.querySelectorAll('.html-quiz-questions-section');
-  const newDiv = document.createElement('div');
   newDiv.classList.add('flex', 'flex-col', 'gap-y-5');
 
   const newAnswers = `
-  <button class="question-btn answer hover:opacity-80 hover:transition first">
+  <button class="question-btn answer first">
     <div class="quiz-icon"><h1 class="quiz-answer-text"><b>A</b></h1></div>
     <h2>
       <p class="question-text">${HTMLAnswersArr[currentQuestion].first}</p>
     </h2>
   </button>
-  <button class="question-btn answer hover:opacity-80 hover:transition second">
+  <button class="question-btn answer second">
     <div class="quiz-icon"><h1 class="quiz-answer-text"><b>B</b></h1></div>
     <h2>
       <p class="question-text">${HTMLAnswersArr[currentQuestion].second}</p>
     </h2>
   </button>
-  <button class="question-btn answer hover:opacity-80 hover:transition third">
+  <button class="question-btn answer third">
     <div class="quiz-icon"><h1 class="quiz-answer-text"><b>C</b></h1></div>
     <h2>
       <p class="question-text">${HTMLAnswersArr[currentQuestion].third}</p>
     </h2>
   </button>
-  <button class="question-btn answer hover:opacity-80 hover:transition fourth">
+  <button class="question-btn answer fourth">
     <div class="quiz-icon"><h1 class="quiz-answer-text"><b>D</b></h1></div>
     <h2>
       <p class="question-text">${HTMLAnswersArr[currentQuestion].fourth}</p>
